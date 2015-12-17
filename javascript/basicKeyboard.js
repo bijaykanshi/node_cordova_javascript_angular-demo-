774e0b85330740742803ef66f3582d0f8dcd15cc
Bijay Kanshi <bijay.iit12@gmail.com>
2015-12-17 21:57:58 +0530

    by Bijay Kanshi

--- javascript/basicKeyboard.js
+++ javascript/basicKeyboard.js
@@ -1,6 +1,6 @@
 var basicKeyboard = {
     settings: {
-        buttonClass: "button",
+        buttonClass: "basicButton",
         onclick: "basicKeyboard.keyTap();",
         keyClass: "key",
         text: {
@@ -10,7 +10,8 @@ var basicKeyboard = {
     keyTap: function (value) {
         var field = basicKeyboard.field,
             startPos,
-            endPos;
+            endPos,
+            bClass;
         if (field.selectionStart >= 0) {
             startPos = field.selectionStart;
             endPos = field.selectionEnd;
@@ -30,31 +31,52 @@ var basicKeyboard = {
         var onclick = "",
             text = "",
             div = "";
-        div += "<div id=\"keyboard\"><div id=\"keyboardHeader\"></div>";
+        div += "<div id=\"basicKey\"><div id=\"keyboardHeader\"></div>";
         div += "<div id=\"keyboardCapitalLetter\">";
         basicKeyboard.defaultKeyboard.capitalLetter.forEach(function (key) {
             generate(key);
         });
         div += "</div>";
         function generate(key) {
+            bClass = key.buttonClass == undefined ? basicKeyboard.settings.buttonClass : key.buttonClass;
             onclick = key.onclick === undefined ? basicKeyboard.settings.onclick.replace("()", "(" + key.value + ")") : key.onclick;
             text = (key.isChar !== undefined || key.isChar === false) ? key.value : String.fromCharCode(key.value);
-            div += "<button class='basicButton' onclick=\"" + onclick + "\"><div class='key'>" + text + "</div></button>";
+            div += "<button class=\"" + bClass + "\" onclick=\"" + onclick + "\"><div class='key'>" + text + "</div></button>";
             onclick = "";
             text = "";
+            bClass = "";
         }
         document.getElementById(basicKeyboard.keyboardLayout).innerHTML = div;
+    },
+    deleteFun: function() {
+        var field = basicKeyboard.field,
+            startPos = field.selectionStart,
+            endPos = field.selectionEnd;
+            if (field.selectionStart === field.selectionEnd) {
+                field.value = field.value.substring(0, startPos - 1) + field.value.substring(endPos, field.value.length);
+                field.focus();
+                field.setSelectionRange(startPos - 1, startPos - 1);
+            } else {
+                field.value = field.value.substring(0, startPos) + field.value.substring(endPos, field.value.length);
+                field.focus();
+                field.setSelectionRange(startPos, startPos);
+            }
     },
     defaultKeyboard: {
         capitalLetter:
             [
                 { value: 81 }, { value: 87 }, { value: 69 }, { value: 82 }, { value: 84 }, { value: 89 },
                 { value: 85 }, { value: 73 }, { value: 79 }, { value: 80 },
-                { value: 65, buttonClass: "button button_a" }, { value: 83 }, { value: 68 }, { value: 70 },
+                { value: "Del", isChar: "false", onclick: "basicKeyboard.deleteFun()" },
+                { value: 65, buttonClass: "basicButton secondRow" }, { value: 83 }, { value: 68 }, { value: 70 },
                 { value: 71 }, { value: 72 }, { value: 74 }, { value: 75 }, { value: 76 },
                 { value: 90 }, { value: 88 }, { value: 67 }, { value: 86 }, { value: 66 }, { value: 78 },
-                { value: 77 }, { value: 44 }, { value: 46 }, { value: 39, buttonClass: "button button_quote" },
-                { value: 32, buttonClass: "button button_space" }
+                { value: 77 }, { value: 44 }, { value: 46 }, { value: 39 },
+                { value: 32, buttonClass: "basicButton button_space" }
             ]
     }
+};
+window.onload = function() {
+   basicKeyboard.initialize('keyboard');
+   basicKeyboard.field = document.getElementById('writeArea');
 };
